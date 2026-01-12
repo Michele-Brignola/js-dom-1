0ca5b9fa0e4d2f1b33d1ad8f83d5121a44cf846b
Michele-Brignola <michele.brignola@hotmail.com>
2026-01-12 17:27:48 +0100

    added bonus

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 const lamp = document.getElementById("lamp");
 const button = document.getElementById("switch");
+let isLampOn = false;
 
 button.addEventListener("click", function () {
-  lamp.src="../img/yellow_lamp.png"
+  if (isLampOn) {
+    lamp.src = "../img/white_lamp.png";
+    isLampOn = false;
+    button.innerHTML = "Accendi"
+  } else {
+    lamp.src = "../img/yellow_lamp.png";
+    isLampOn = true;
+    button.innerHTML = "Spegni"
+  }
 });
