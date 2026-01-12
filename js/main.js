0447d6e74a14baaca0284f41c4048a8cb103de69
Michele-Brignola <michele.brignola@hotmail.com>
2026-01-12 17:44:31 +0100

    added bonus²

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
 const lamp = document.getElementById("lamp");
 const button = document.getElementById("switch");
+const lampOff = "../img/white_lamp.png"
+const lampOn = "../img/yellow_lamp.png"
 let isLampOn = false;
 
+// Versione 1 con isLampOn
 button.addEventListener("click", function () {
   if (isLampOn) {
-    lamp.src = "../img/white_lamp.png";
+    lamp.src = lampOff;
     isLampOn = false;
-    button.innerHTML = "Accendi"
+    button.textContent = "Accendi"
   } else {
-    lamp.src = "../img/yellow_lamp.png";
+    lamp.src = lampOn;
     isLampOn = true;
-    button.innerHTML = "Spegni"
+    button.textContent = "Spegni"
   }
 });
+
+// Versione 2 con controllo src
+// button.addEventListener("click", function () {
+//   if (lamp.src.includes("white_lamp.png")) {
+//     lamp.src = lampOn;
+//     button.textContent = "Spegni"
+//   } else {
+//     lamp.src = lampOff;
+//     button.textContent = "Accendi"
+//   }
+// });
