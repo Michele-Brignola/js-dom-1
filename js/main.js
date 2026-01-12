de4ee4b65184b599abdf47755f770d97862ef85e
Michele-Brignola <michele.brignola@hotmail.com>
2026-01-12 15:45:25 +0100

    added base

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const lamp = document.getElementById("lamp");
+const button = document.getElementById("switch");
+
+button.addEventListener("click", function () {
+  lamp.src="../img/yellow_lamp.png"
+});
